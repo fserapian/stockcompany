ae7dff9acdf524d0a46bdd1df35bf5a7a69dfce9
Fadi Serapian <fadi.serapian@gmail.com>
2021-04-07 19:36:44 -0400

    add font-awesome with npm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <!-- font awesome CDN -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css">
         <!-- main css style -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         {{-- <!-- responsive css style -->
